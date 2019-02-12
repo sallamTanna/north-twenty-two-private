ea1fce180e3fefda2c0ee25a3583395b080762af
unknown <sallamtanna2015@hotmail.com>
2019-02-12 23:30:46 +0200

    Displaying cart menu

--- client/public/js/menuComponent.js
+++ client/public/js/menuComponent.js
@@ -1,4 +1,3 @@
-var body = document.getElementsByTagName('body')[0];
 var html = document.getElementsByTagName('html')[0];
 var linksBox = document.getElementsByClassName('links-box')[0];
 var hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0];
@@ -7,15 +6,18 @@ var subMenuBtn = document.getElementsByClassName('sub-menu-btn')[0];
 var subMenu = document.getElementsByClassName('sub-menu')[0];
 var navFooter = document.getElementsByClassName('nav-footer')[0];
 var closeIcon = document.getElementsByClassName('colse-icon')[0];
+var cartsBox = document.getElementsByClassName('carts-box')[0];
+var cartIcon = document.getElementsByClassName('cart-icon')[0];
 
-body.addEventListener('click', (e)=>{
+html.addEventListener('click', (e)=>{
 
   if (window.innerWidth > 1024) {
     closeIcon.style.display = "none";
   }
 
   // To prevent closing the "linksBox" then we click on it, and close it whereever we click
-  if(e.target == linksBox || e.target == mainUl || e.target == navFooter || e.target == html) {
+  if(e.target == linksBox || e.target == mainUl || e.target == navFooter || e.target == cartsBox) {
+     e.stopPropagation();
   }
 
   else if(e.target == subMenuBtn) {
@@ -29,5 +31,7 @@ body.addEventListener('click', (e)=>{
   else {
     linksBox.style.display = "none";
     hamburgerIcon.style.display = "block";
+    cartsBox.style.display = "none";
+    cartIcon.style.display = "block";
   }
 })
