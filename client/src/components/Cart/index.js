727cb5add9a733f0f037a92a5ee7e0062c515c73
unknown <sallamtanna2015@hotmail.com>
2019-02-12 20:49:11 +0200

    some fixing

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -8,7 +8,8 @@ class Cart extends Component {
 
   openCartMenu = ()=>{
     console.log('ddddddddd');
-    console.log('yyyyyyyyyyy');
+    var cartsBox = document.getElementsByClassName('carts-box')[0];
+    cartsBox.style.display = "block";
   }
 
   render() {
@@ -16,28 +17,10 @@ class Cart extends Component {
       <span className="cart-text">cart</span>
       <span>{this.state.cartNumber}</span>
 
-      <nav className="links-box">
-        <section className="colse-icon" onClick={this.closeMenu}>
-          <i class="fas fa-times-circle"></i>
-        </section>
-        <ul className="main-ul">
-          <li><a href="#">HOME</a></li>
-          <li className="sub-menu-btn">WATCHES</li>
-          <div className="sub-menu">
-            <li><a>ALL WATCHES</a></li>
-            <li><a>WOMEN'S</a></li>
-            <li><a>MEN'S</a></li>
-          </div>
-          <li><a href="#">WRISTBANDS</a></li>
-          <li><a href="#">OUR STORY</a></li>
-          <li><a href="#">#NORTHTWENTYTWO</a></li>
-          <li><a href="#">JOURNAL</a></li>
-          <li><a href="#">SUPPORT</a></li>
-        </ul>
-        <footer className="nav-footer">
-          © 2017 GILLBO GROUP AB. ALL RIGHTS RESERVED.
-        </footer>
-      </nav>
+      <section className="carts-box">
+    
+      </section>
+
     </section>
   }
 }
