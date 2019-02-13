5b388fc228235d77a036b5078c994ce78961b8ce
unknown <sallamtanna2015@hotmail.com>
2019-02-13 23:31:58 +0200

    Finishing oneCart component

--- client/src/components/OneCart/index.js
+++ client/src/components/OneCart/index.js
@@ -4,7 +4,16 @@ import './style.css';
 class OneCart extends Component {
 
   render(){
-    return <section>
+    return <section className="one-cart-main-section">
+
+      <i class="fas fa-times"></i>
+      <img src={this.props.src} />
+      <div className="cart-content">
+        <span>{this.props.name}</span>
+        <span>Unit Price: ${this.props.price}</span>
+        <span>Quantity: {this.props.quantity}</span>
+      </div>
+
     </section>
   }
 }
