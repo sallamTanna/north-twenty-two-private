75896d24632f2d6e14b2e6b08555a78ad85fbfe8
unknown <sallamtanna2015@hotmail.com>
2019-02-20 00:43:05 +0200

    Creating MainImage component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Header from './Header';
+import MainImage from './MainImage';
 import './App.css';
 
 class App extends React.Component {
@@ -8,6 +9,7 @@ class App extends React.Component {
     return <div className="app">
       <Header />
       <p className="app-title">SECURE PAYMENTS & SHIPPING WORLD WIDE</p>
+      <MainImage />
     </div>;
   }
 }
---
+++ client/src/components/MainImage/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class MainImage extends Component {
+render(){return <section className="main-image-main-section">
+  <div></div>
+</section>}
+}
+
+export default MainImage;
