1af617b0afd4d7f6f673dc850b3def845f991e20
unknown <sallamtanna2015@hotmail.com>
2019-02-20 19:33:06 +0200

    Add some more details and style

--- client/src/components/MainImage/index.js
+++ client/src/components/MainImage/index.js
@@ -3,7 +3,11 @@ import './style.css';
 
 class MainImage extends Component {
 render(){return <section className="main-image-main-section">
-  <div></div>
+  <div>
+  <p>CRAFTED FOR THE HARSH AND VARIED YET BEAUTIFUL CLIMATE OF THE NORTH</p>
+  <p>Minimalistic Watches</p>
+  <p><a>SHOP NOW</a></p>
+  </div>
 </section>}
 }
 
