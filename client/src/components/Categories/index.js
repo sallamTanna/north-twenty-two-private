cef0289225e433e9a5984b8ee3a0f106ee74c103
unknown <sallamtanna2015@hotmail.com>
2019-02-20 22:08:34 +0200

    Creating the Categories component folder

---
+++ client/src/components/Categories/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Categories extends Component {
+  render(){
+    return <section className="component-main-section">
+    </section>
+  }
+}
+
+export default Categories;
