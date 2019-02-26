7716ad7e9ae24e5332fb2088abf18e4a46e32b2d
unknown <sallamtanna2015@hotmail.com>
2019-02-26 19:57:27 +0200

    Fixing the links

---
+++ client/src/components/OurStory/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class OurStory extends Component {
+  render(){
+    return <section className="ourStory-main-section">
+      <div>
+        <span></span>
+        <p>BUILT TO LAST</p>
+        <h1>Minimalism & Quality</h1>
+        <p>We create minimalistic and qualitative wristwear for every individual and occasion. Our team is based in Stockholm, where we design, partially assemble and package all timepieces by hand.</p>
+        <a>read more</a>
+      </div>
+
+      <div></div>
+    </section>
+  }
+}
+
+export default OurStory;
