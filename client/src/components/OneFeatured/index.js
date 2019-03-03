582ab6e2cdd69ee8ab6db81839f40b11931e1eb4
unknown <sallamtanna2015@hotmail.com>
2019-03-04 00:55:20 +0200

    Creating the OneFeatured  component

---
+++ client/src/components/OneFeatured/index.js
@@ -0,0 +1,10 @@
+import React, { Component } from react;
+import '/style.css';
+
+class OneFeatured extends Component {
+  render() {
+    return <section className="one-featured-main-section"></section>
+  }
+}
+
+export default OneFeatured;
