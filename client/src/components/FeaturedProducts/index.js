f595310f396b39f7ea970db2a1afc147c47831f2
unknown <sallamtanna2015@hotmail.com>
2019-03-04 23:32:12 +0200

    Finishing FeaturedProducts component

--- client/src/components/FeaturedProducts/index.js
+++ client/src/components/FeaturedProducts/index.js
@@ -4,13 +4,13 @@ import './style.css';
 
 class FeaturedProducts extends Component {
   state = {
-    products: [{name:'sallam', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2016/06/Bergen-Rose-2-500x500.jpg', href:'test'}]
+    products: [{name:'Dana', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2016/06/Bergen-Rose-2-500x500.jpg',          href:'test'}, {name:'Lana', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2017/01/Vaxholm-S.jpg', href:'test'}, {name:'Salam', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2017/08/Oslo-Ros-36mm-500x500.jpg', href:'test'}, {name:'Reema', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2017/01/Svarts%C3%B6-M-1-500x500.jpg', href:'test'}],
   }
 
   render(){
     return <section className="featured-products-main-section">
+    <p>Featured Products</p>
     {this.state.products.map(product => <OneFeatured name={product.name} price={product.price} src={product.src} href={product.href} />)}
-
     </section>
   }
 }
