649e5520d3ff82a9784e31594cc88ca23b388f65
unknown <sallamtanna2015@hotmail.com>
2019-03-04 20:17:08 +0200

    Finishing the OneFeatured component

--- client/src/components/FeaturedProducts/index.js
+++ client/src/components/FeaturedProducts/index.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import OneFeatured from '../OneFeatured';
 import './style.css';
 
 class FeaturedProducts extends Component {
+  state = {
+    products: [{name:'sallam', price:1.395, src:'https://northtwentytwo.com/wp-content/uploads/2016/06/Bergen-Rose-2-500x500.jpg', href:'test'}]
+  }
+
   render(){
     return <section className="featured-products-main-section">
+    {this.state.products.map(product => <OneFeatured name={product.name} price={product.price} src={product.src} href={product.href} />)}
+
     </section>
   }
 }
--- client/src/components/OneFeatured/index.js
+++ client/src/components/OneFeatured/index.js
@@ -1,9 +1,20 @@
-import React, { Component } from react;
-import '/style.css';
+import React, { Component } from 'react';
+import './style.css';
 
 class OneFeatured extends Component {
   render() {
-    return <section className="one-featured-main-section"></section>
+    return <section className="one-featured-main-section">
+      <li>
+        <figure>
+          <img src={this.props.src} />
+          <div className="select-option-button"><span>select options</span></div>
+        </figure>
+        <div className="more-details">
+          <h3><a href={this.props.href}>{this.props.name}</a></h3>
+          <span>{this.props.price}<span className="lighter-span"> KR</span></span>
+        </div>
+      </li>
+    </section>
   }
 }
 
