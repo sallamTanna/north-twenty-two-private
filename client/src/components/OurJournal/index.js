c978b75d32bbcf038b61d81810277d11d9c1e039
unknown <sallamtanna2015@hotmail.com>
2019-03-04 23:37:54 +0200

    Creating OurJournal component folder

---
+++ client/src/components/OurJournal/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class OurJournal extends Component {
+  render() {
+    return <section className="our-journal-main-section">
+    </section>
+  }
+}
+
+export default OurJournal;
