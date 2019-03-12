093e3e0286f09db3b1a683ec4990131b40f83b2c
unknown <sallamtanna2015@hotmail.com>
2019-03-12 23:31:11 +0200

    create pages

---
+++ client/src/pages/Home.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Header from '../components/Header';
+import MainImage from '../components/MainImage';
+import Categories from '../components/Categories';
+import OurStory from '../components/OurStory';
+import FeaturedProducts from '../components/FeaturedProducts';
+import OurJournal from '../components/OurJournal';
+import BeUpdated from '../components/BeUpdated';
+import Footer from '../components/Footer';
+
+class Home extends React.Component {
+
+  render() {
+    return <main>
+      <MainImage />
+      <Categories />
+      <OurStory />
+      <FeaturedProducts />
+      <OurJournal />
+      <BeUpdated />
+      <Footer />
+    </main>
+  }
+}
+
+export default Home;
