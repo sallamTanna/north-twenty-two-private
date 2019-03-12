c7d0fbe03254183940cfd107fe403ec9a39b0a71
unknown <sallamtanna2015@hotmail.com>
2019-03-12 23:36:10 +0200

    Creating the OurStoryNavigationMenu component

---
+++ client/src/components/OurStoryNavigationMenu/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class OurStoryNavigationMenu extends Component {
+  render() {
+    return <section className="ourStoryNavigationMenu-main-section">
+      salalm
+    </section>
+  }
+}
+
+export default OurStoryNavigationMenu;
--- client/src/pages/OurStory.js
+++ client/src/pages/OurStory.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 import OurStoryAboutUs from '../components/OurStoryAboutUs';
 import OurStoryOurMission from '../components/OurStoryOurMission';
 import OurStoryProduction from '../components/OurStoryProduction';
+import OurStoryNavigationMenu from '../components/OurStoryNavigationMenu';
 import Footer from '../components/Footer';
 
 class OurStory extends Component {
   render() {
     return <main>
+      <OurStoryNavigationMenu />
       <OurStoryAboutUs />
       <OurStoryOurMission />
       <OurStoryProduction />
