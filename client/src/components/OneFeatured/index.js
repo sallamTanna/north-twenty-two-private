9181b5388c265eed8a7996c6be4f9f4eb7513b3c
unknown <sallamtanna2015@hotmail.com>
2019-03-26 20:46:50 +0200

    Fixing the oneFeature component, so the user can go to the detailedPage when he clicks the component

--- client/src/components/OneFeatured/index.js
+++ client/src/components/OneFeatured/index.js
@@ -5,14 +5,19 @@ class OneFeatured extends Component {
   render() {
     return <section className="one-featured-main-section">
       <li>
-        <figure>
-          <img src={this.props.src} />
-          <div className="select-option-button"><span>select options</span></div>
-        </figure>
-        <div className="more-details">
-          <h3><a href={this.props.href}>{this.props.name}</a></h3>
-          <span>{this.props.price}<span className="lighter-span"> KR</span></span>
-        </div>
+          <figure>
+            <img src={this.props.src} />
+            <a href={this.props.href}>
+              <div className="select-option-button">
+              <span>select options</span>
+              </div>
+            </a>
+          </figure>
+          
+          <div className="more-details">
+              <h3>{this.props.name}</h3>
+              <span>{this.props.price}<span className="lighter-span"> KR</span></span>
+          </div>
       </li>
     </section>
   }
