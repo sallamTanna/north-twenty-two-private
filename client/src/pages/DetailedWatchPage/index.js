20a5dd46d230f1f0276d63745c7c1d7631442e0c
unknown <sallamtanna2015@hotmail.com>
2019-03-31 00:13:06 +0300

    Fixing the text on the button

--- client/src/pages/DetailedWatchPage/index.js
+++ client/src/pages/DetailedWatchPage/index.js
@@ -8,16 +8,18 @@ class DetailedWatchPage extends Component {
     selectedImage: '',
     wristbandColors:['#d9cba1', 'black'],
     selectedWristbandColor:'',
+    buttonText: 'add to cart',
   }
 
   changeSelectedImage = (index) => this.setState({ selectedImage: this.state.watchCollection[index] })
   changeSelectedWristbandColor = (color) => this.setState({selectedWristbandColor:color})
+  changeButtonText = () => this.setState({buttonText: 'item added'})
 
   render() {
     return <main className="detailed-watch-page">
       <WatchImages src={this.state.selectedImage || this.state.watchCollection[0]} watchCollection={this.state.watchCollection} selectedImage={(index)=> this.changeSelectedImage(index)} />
 
-      <WatchSummary watchName={'sallam'} watchPrice={'15'} summary={'Saltarö is inspired by the Stockholm archipelago which is seen in many aspects of the design. Powered by a Swiss Ronda movement then protected by 316l stainless steel and dome sapphire crystal with anti reflective coating. The brushed light rose gold case combined with a white face gives the timepiece a refined look. Available with either a magnetic mesh or genuine Italian leather wristband.'} wristbandColors={this.state.wristbandColors} changeSelectedWristbandColor={(color)=>this.changeSelectedWristbandColor(color)} wristbandColor={this.state.selectedWristbandColor}/>
+      <WatchSummary watchName={'sallam'} watchPrice={'15'} summary={'Saltarö is inspired by the Stockholm archipelago which is seen in many aspects of the design. Powered by a Swiss Ronda movement then protected by 316l stainless steel and dome sapphire crystal with anti reflective coating. The brushed light rose gold case combined with a white face gives the timepiece a refined look. Available with either a magnetic mesh or genuine Italian leather wristband.'} wristbandColors={this.state.wristbandColors} changeSelectedWristbandColor={(color)=>this.changeSelectedWristbandColor(color)} wristbandColor={this.state.selectedWristbandColor} buttonText={this.state.buttonText} changeButtonText={()=>this.changeButtonText()} />
     </main>
   }
 }
