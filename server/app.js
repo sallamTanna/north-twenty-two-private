01b136c31b4f556786f4deaf0b33b076a5a4f004
SallamTanna <sallamtanna2015@hotmail.com>
2021-04-11 22:42:07 +0300

    Update app.js

--- server/app.js
+++ server/app.js
@@ -5,12 +5,13 @@ const cookieParser = require('cookie-parser');
 const controllers = require('./controllers/index');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser(process.env.SECRET));
 app.use( controllers);
 app.set('port', process.env.PORT || 3001);
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
 });
